6d2294faac38db63531467d38eaf42e6a41423bb
411-11-Dachpiput <54293@yupparaj.ac.th>
2026-09-03 20:14:41 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -99,7 +99,7 @@ ans2 = st.text_input(
     value=st.session_state.ans2_val,
 )
 ans3 = st.text_input(
-    "ข้อ 3: He is going to buy new `c _ _ p _ t e r`. 🍎",
+    "ข้อ 3: He is going to buy new `c _ _ p _ t e r`. 🖥️",
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
