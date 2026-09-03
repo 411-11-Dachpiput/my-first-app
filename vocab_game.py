5454881a407a3b479f1c4b72fb145a1140dab585
411-11-Dachpiput <54293@yupparaj.ac.th>
2026-09-03 20:12:01 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -127,7 +127,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2, ans3, ans4)
+    show_result_dialog(ans1, ans2)
 
 st.divider()
 st.write("นายเดชพิภัช คลืนสันเทียะ เลขที่ 11 ม.4/11")
